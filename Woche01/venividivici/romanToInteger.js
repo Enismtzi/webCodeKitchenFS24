1cf4116b11a82832f273d79ee415cabd819248b2
enis <enis.murtezi@students.fhnw.ch>
2024-02-25 13:27:47 +0100

    Commit 04: Roman to integer Solution for 2 decimals

--- Woche01/venividivici/romanToInteger.js
+++ Woche01/venividivici/romanToInteger.js
@@ -4,28 +4,38 @@
 /*jshint -W097 */
 
 function convertRomanToInteger() {
+
+    // get the input value
     const romanInput = document.getElementById("romanNumber").value.toLowerCase();
 
+    // dictionary for the roman values
     const romanValues = {
         "i": 1, "v": 5, "x": 10, "l": 50, "c": 100, "d": 500, "m": 1000
     };
 
+    // convert the input to an array
     const romanArray = [...romanInput];
 
-    //logic for 1-10
-    let sum = 0;
-    // recursiv approach
-    for (let i = romanArray.length; i > 0; i--) {
-        if (i === 1) {
-            sum += romanValues[romanArray[i - 1]];
-        } (i === 2) {
-            // check if addition or subtraction
 
-
+    // convert the roman number to an integer
+    let sum;
+    const arrayLength = romanArray.length;
+    if (arrayLength === 1) {
+        sum = romanValues[romanArray[arrayLength - 1]];
+    } else if (arrayLength === 2) {
+        const index0 = romanValues[romanArray[arrayLength - 2]];
+        const index1 = romanValues[romanArray[arrayLength - 1]];
+        if (index0 >= index1) {
+            sum = index0 + index1;
+        } else {
+            sum = index1 - index0;
         }
-
+    } else {
+        sum = "not implemented yet!";
     }
 
-    document.getElementById("convertedOutput").innerText = "Integer Value: " + romanInput.toLowerCase();
+    document.getElementById("convertedOutput").innerText = "Integer Value: " + sum;
+
+    sum = 0;
 
 }
