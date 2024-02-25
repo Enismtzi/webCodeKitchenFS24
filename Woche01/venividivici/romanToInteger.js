4eae55d2dbb553bed6d1da353af20e910cabb7af
enis <enis.murtezi@students.fhnw.ch>
2024-02-25 13:03:22 +0100

    Commit 03: Roman to Integer approach 1

--- Woche01/venividivici/romanToInteger.js
+++ Woche01/venividivici/romanToInteger.js
@@ -1,5 +1,31 @@
+/*jshint esversion: 6 */
+"use strict";
 
-function convertRomanToInteger(){
-    document.getElementById("convertedOutput").innerText = "Integer Value: " + "12321141";
+/*jshint -W097 */
+
+function convertRomanToInteger() {
+    const romanInput = document.getElementById("romanNumber").value.toLowerCase();
+
+    const romanValues = {
+        "i": 1, "v": 5, "x": 10, "l": 50, "c": 100, "d": 500, "m": 1000
+    };
+
+    const romanArray = [...romanInput];
+
+    //logic for 1-10
+    let sum = 0;
+    // recursiv approach
+    for (let i = romanArray.length; i > 0; i--) {
+        if (i === 1) {
+            sum += romanValues[romanArray[i - 1]];
+        } (i === 2) {
+            // check if addition or subtraction
+
+
+        }
+
+    }
+
+    document.getElementById("convertedOutput").innerText = "Integer Value: " + romanInput.toLowerCase();
 
 }
